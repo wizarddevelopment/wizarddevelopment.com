f2a16e1a1d44debb42c9d256c21f55b8e7211e4a
Francis Gulotta <wizard@roborooter.com>
2013-11-15 10:50:45 -0500

    Add contact requests database table
    I want to save these things for the future

---
+++ db/migrate/20131113202654_create_contact_requests.rb
@@ -0,0 +1,13 @@
+class CreateContactRequests < ActiveRecord::Migration
+  def change
+    create_table :contact_requests do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :business_name
+      t.text :message
+      t.text :project_info
+      t.timestamps
+    end
+  end
+end
