aac4bc63c76a42c1ac3bafa06a556d425d822043
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-06-06 16:09:43 -0400

    Ember on Rails Yo

--- app/assets/javascripts/mvp.js
+++ app/assets/javascripts/mvp.js
@@ -1,18 +1,70 @@
-// this runs on /mvp
-
 
 App = Ember.Application.create();
 
-App.GustavoComponent = Ember.Component.extend({
-  tagName: 'gustavo',
-  src: 'gustavo.jpg',
-  name: 'Gus!'
+App.WizardGusComponent = Ember.Component.extend({
+  pic: 'assets/gus.jpg',
+  name: 'Gus!',
+  about: 'Not to mention, funny, outgoing, NOT retarded, and a great basketball team player that puts Amy and Armando to division 10 level'
+});
+
+App.WizardArmandoComponent = Ember.Component.extend({
+  pic: 'assets/armando.jpg',
+  name: 'Armando!',
+  about: 'He is a great programmer and the best basketball player in Chinatown'
+});
+
+App.WizardHannahComponent = Ember.Component.extend({
+  pic: 'assets/hannah.jpg',
+  name: 'Hannah!',
+  about: 'She is CHO(Chief Happiness Officer) and Front End Developer'
+});
+
+App.WizardFrancisComponent = Ember.Component.extend({
+  pic: 'assets/francis.jpg',
+  name: 'Francis!',
+  about: 'He is robo cat!'
+});
+
+App.WizardAmyComponent = Ember.Component.extend({
+  pic: 'assets/amy.png',
+  name: 'Amy!',
+  about: 'Handlebars wizard!! Can use them for carrying logs or rendering content --amazing!'
 });
 
 App.Router.reopen({
   location: 'history'
 });
 
-App.Router.map(function(){
-  this.route('mvp');
+//Start Ugly Routes? Note Research about Routes in Ember and Rails
+App.TeamRoute = Ember.Route.extend({
+  model: function(){
+    return '/team';
+  }
 });
+
+App.BusinessRoute = Ember.Route.extend({
+  model: function(){
+    return '/business';
+  }
+});
+
+App.DevelopmentRoute = Ember.Route.extend({
+  model: function(){
+    return '/development';
+  }
+});
+
+App.ContactRoute = Ember.Route.extend({
+  model: function(){
+    return '/contact';
+  }
+});
+//End of Ugly Routes
+
+App.Router.map(function(){
+  this.route('business');
+  this.route('team');
+  this.route('development');
+  this.route('contact');
+});
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 WizardDevelopment::Application.routes.draw do
-  root 'home#index'
-  get 'business' => 'home#index', format: false
-  get 'devteam' => 'home#index', format: false
+  root 'home#mvp'
+  # get 'business' => 'home#index', format: false
+  # get 'devteam' => 'home#index', format: false
 
   resource :contact_requests, only: [:create], format: false
 
-  get 'mvp' => 'home#mvp', format: false
+ get 'team', to: 'home#mvp'
+ get 'business', to: 'home#mvp'
+ get 'development', to: 'home#mvp'
+ get 'contact', to: 'home#mvp'
 end
