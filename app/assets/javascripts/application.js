4164f924d3da2966ca70387619469df3d4158aa9
amyg225 <amyg225@gmail.com>
2014-07-01 15:22:02 -0400

    hero for homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require_tree ./lib
 //= require ./app/app.js
+//= require ./app/navbar.js
 //= require_tree ./app
