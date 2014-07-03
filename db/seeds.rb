a20f6426b9b79529ec44f6984a0da8e3685cb967
Francis Gulotta <wizard@roborooter.com>
2014-07-03 14:43:33 -0400

    A few changes  - Updated images for armando and amy  - renamed everything from team to employees  - dropped the wizard-white - I think it might make sense to have colors for things eg, accent-color  - refactored css to apply columns directly to the bio-box, and have less css overall  - removed the circles as they got messy on small screen sizes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,55 +9,45 @@
 Employee.create(
   name: "Francis Gulotta",
   email: "francis@wizarddevelopment.com",
-  twitter: "https://twitter.com/reconbot",
-  github: "https://github.com/reconbot",
+  twitter: "reconbot",
+  github: "reconbot",
   blog: "http://wizard.codes/",
-  bio: "Cupcake ipsum dolor sit amet. Marzipan sweet apple pie chocolate cake brownie chupa chups croissant. Croissant oat cake cotton candy carrot cake chocolate tart topping.",
+  bio: 'Francis is a long time developer and technologist. He\'s run developement teams for Hedge Funds, Tech Startups, and Private Education. He loves teaching, javascript and robots. And has recently spoken at <a href="http://2014.jsconfbr.org/">JSConf Brazil</a>, <a href="http://jqueryuk.com/">jQuery Conf UK</a> and <a href="http://2014.jsconf.us/">JS Conf US</a>.',
   photo: "francis_gulotta.jpg"
-  )
+)
 
 Employee.create(
   name: "Hannah Nordgren",
   email: "hannah@wizarddevelopment.com",
-  twitter: "https://twitter.com/hnsayshello",
-  github: "https://github.com/ninjaofawesome",
+  twitter: "hnsayshello",
+  github: "ninjaofawesome",
   blog: "http://wizard.codes/",
   bio: "Hannah loves making beautiful products for interesting people.  She enjoys aesthetics both in front of the screen as a designer and behind it as a developer.  She also likes bikes (a lot.)",
   photo: "hannah.jpg"
-  )
+)
 
 Employee.create(
   name: "Armando Amador",
   email: "armando@wizarddevelopment.com",
-  github: "https://github.com/ArmandoAmador",
+  github: "ArmandoAmador",
   blog: "http://wizard.codes/",
-  bio: "Cupcake ipsum dolor sit amet. Marzipan sweet apple pie chocolate cake brownie chupa chups croissant. Croissant oat cake cotton candy carrot cake chocolate tart topping.",
-  photo: "monkeynaut.jpg"
-  )
+  photo: "armando.jpg"
+)
 
 Employee.create(
   name: "Amy Gimma",
   email: "amy@wizarddevelopment.com",
-  twitter: "https://twitter.com/amyg225",
-  github: "https://github.com/amyg225",
+  twitter: "amyg225",
+  github: "amyg225",
   blog: "http://wizard.codes/",
-  bio: "Cupcake ipsum dolor sit amet. Marzipan sweet apple pie chocolate cake brownie chupa chups croissant. Croissant oat cake cotton candy carrot cake chocolate tart topping.",
   photo: "amy.jpg"
-  )
+)
 
 Employee.create(
   name: "Gustavo Guimares",
-  twitter: "https://twitter.com/gus_tavo_guim",
+  twitter: "gus_tavo_guim",
   email: "gustavo@wizarddevelopment.com",
-  github: "https://github.com/gustavoguimaraes",
+  github: "gustavoguimaraes",
   blog: "http://wizard.codes/",
-  bio: "You can sum Gus up in one word: Fabuloso.",
   photo: "gus.jpeg"
-  )
-
-Employee.create(
-  name: "Maybe You?",
-  email: "team@wizarddevelopment.com",
-  bio: "We like meeting passionate, personable developers at all levels.  Think you might be a good fit for Wizard?  Get in touch, we'd love to have coffee.",
-  photo: "positive-people.jpg"
-  )
+)
